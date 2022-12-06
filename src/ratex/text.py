28558aff9519d33aa1453e1ab93108cc16dc251c
tristan <heytristaann@gmail.com>
2022-12-06 18:15:21 -0500

    fixed text bug

--- src/ratex/text.py
+++ src/ratex/text.py
@@ -8,16 +8,22 @@ class Equation(object):
 
     def __str__(self) -> str:
         self.content = self.content.replace(" ", "\\;")
-        type: str = '*' if not self.enumerate else ''
+        type: str = '*'  if not self.enumerate else ''
         return f"\\begin{{equation{type}}}{self.content}\\end{{equation{type}}}"
 
 # // Text class
 class Text(object):
     def __init__(self, content: str, b: bool = False, it: bool = False) -> None:
         self.content = re.sub("\s\s+", " ", content).strip()
-        self.b = b
-        self.it = it
+        self.bold = b
+        self.italic = it
     
     def __str__(self) -> str:
-        return f"\\text{'bf' if self.b else 'it' if self.it else ''}{{{self.content}}}"
+        if self.bold:
+            return f"\\textbf{{{self.content}}}"
+        elif self.italic:
+            return f"\\textit{{{self.content}}}"
+        elif self.italic and self.bold:
+            return f"\\textbf{{\\textit{{{self.content}}}}}"
+        return f"{{{self.content}}}"
     
